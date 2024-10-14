c325646ea4442325de528b7519479630fb1037cc
rhinosaur0 <paulkdong@gmail.com>
2024-10-14 00:16:50 -0400

    Update ai_detection.py

--- ai_detection.py
+++ ai_detection.py
@@ -1,136 +1,158 @@
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, Dataset
-from torchvision import transforms
+from torch.utils.data import DataLoader, Dataset, Subset
+from torchvision import datasets, transforms
 from PIL import Image
+from tqdm.auto import tqdm
 # import matplotlib.pyplot as plt 
 # uncomment the above to visualize the images
 
-transform = transforms.Compose([
-    transforms.Resize((224, 224)),
+train_transform_trivial_augment = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.TrivialAugmentWide(num_magnitude_bins = 31),
     transforms.ToTensor()
 ])
 
-class CustomImageDataset(Dataset):
-    def __init__(self, root_dir, transform = None):
-        self.root_dir = root_dir
-        self.transform = transform
-        self.image_paths = []
-        self.labels = []
+test_transform = transforms.Compose([
+    transforms.Resize((64, 64)),
+    transforms.ToTensor()
+])
 
-        for label, subdir in enumerate(['FAKE', 'REAL']): # iterates through the fake subdir and the real subdir
-            subdir_path = os.path.join(self.root_dir, subdir)
-            for filename in os.listdir(subdir_path)[:3000]:
-                self.image_paths.append(os.path.join(subdir_path, filename))
-                self.labels.append(label)
+train_data_augmented = datasets.ImageFolder(train_dir, transform = train_transform_trivial_augment)
+test_data_simple = datasets.ImageFolder(test_dir, transform = test_transform)
 
-    def __len__(self):
-        return len(self.image_paths)
+indices_train = torch.randperm(len(train_data_augmented))[:10000]
+sampled_train_data = Subset(train_data_augmented, indices_train)
+indices_test = torch.randperm(len(test_data_simple))[:2000]
+sampled_test_data = Subset(test_data_simple, indices_test)
 
-    def __getitem__(self, idx):
-        img_path = self.image_paths[idx]
-        image = Image.open(img_path).convert('RGB')
-        label = self.labels[idx]
+batch_size = 32
+num_workers = os.cpu_count()
 
-        if self.transform:
-            image = self.transform(image)
-
-        return image, label
-
-img_dir = '/content/train'
-train_data = CustomImageDataset(root_dir = img_dir, transform = transform)
-train_loader = DataLoader(
-    train_data,
-    batch_size = 32,
+torch.manual_seed(42)
+train_augment_loader = DataLoader(
+    sampled_train_data,
+    batch_size = batch_size,
+    num_workers = num_workers,
     shuffle = True
 )
 
-img_dir = '/content/test'
-test_data = CustomImageDataset(root_dir = img_dir, transform = transform)
-test_loader = DataLoader(
-    train_data,
-    batch_size = 32,
-    shuffle = True
+test_dataloader = DataLoader(
+    sampled_test_data,
+    batch_size = batch_size,
+    num_workers = num_workers,
+    shuffle = False
 )
 
-print(len(train_loader), len(test_loader))
-
-import requests
-from pathlib import Path
-
-if Path('helper_functions.py').is_file():
-    print('Skipping download')
-else:
-    print('Downloading helper_functions.py')
-    request = requests.get('https://raw.githubusercontent.com/mrdbourke/pytorch-deep-learning/main/helper_functions.py')
-    with open('helper_functions.py', 'wb') as f:
-        f.write(request.content)
-
-from helper_functions import accuracy_fn
-
-class AIDetection(nn.Module):
-    def __init__(self, input_features, hidden_units, output_features):
+class TinyVGG(nn.Module):
+    def __init__(self, input_shape, hidden_units, output_shape):
         super().__init__()
-        self.chain_layer_1 = nn.Sequential(
-            nn.Conv2d(input_features, hidden_units, kernel_size = 3, stride = 1, padding = 1),
+        self.conv_block_1 = nn.Sequential(
+            nn.Conv2d(input_shape, hidden_units, 3, 1, 1),
             nn.ReLU(),
-            nn.Conv2d(hidden_units, hidden_units, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
+            nn.MaxPool2d(2, 2)
         )
 
-        self.chain_layer_2 = nn.Sequential(
-            nn.Conv2d(hidden_units, hidden_units, kernel_size = 3, stride = 1, padding = 1),
+        self.conv_block_2 = nn.Sequential(
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
             nn.ReLU(),
-            nn.Conv2d(hidden_units, hidden_units, kernel_size = 3, stride = 1, padding = 1),
+            nn.Conv2d(hidden_units, hidden_units, 3, 1, 1),
             nn.ReLU(),
-            nn.MaxPool2d(kernel_size = 2, stride = 2)
+            nn.MaxPool2d(2, 2)
         )
 
         self.classifier = nn.Sequential(
             nn.Flatten(),
-            nn.Linear(in_features = hidden_units * 56 * 56, out_features = 1)
+            nn.Linear(hidden_units * 16 * 16, output_shape)
         )
 
     def forward(self, x):
-        return self.classifier(self.chain_layer_2(self.chain_layer_1(x)))
+        return self.classifier(self.conv_block_2(self.conv_block_1(x)))
 
-model_0 = AIDetection(3, 16, 1)
-print(model_0)
+def train_step(model, dataloader, loss_fn, optimizer):
+    model.train()
 
-loss_fn = nn.BCEWithLogitsLoss()
-optimizer = torch.optim.SGD(model_0.parameters(), lr = 0.1)
+    train_loss, train_acc = 0, 0
+    for batch, (img, label) in enumerate(dataloader):
+        img, label = img.to(device), label.to(device)
 
-epochs = 3
+        label_logits = model(img).squeeze()
 
-for epoch in range(epochs):
-    model_0.train()
-    train_loss = 0
-    acc = 0
-    for img, label in train_loader:
-        y_pred = model_0(img)
-
-        loss = loss_fn(y_pred.squeeze(), torch.tensor(label, dtype = torch.float))
-        acc += accuracy_fn(torch.round(y_pred), torch.tensor(label))
+        loss = loss_fn(label_logits, label.float())
         train_loss += loss
         optimizer.zero_grad()
         loss.backward()
         optimizer.step()
-    train_loss /= len(train_loader)
-    acc /= len(train_loader)
 
-    model_0.eval()
+        label_pred_class = torch.round(torch.sigmoid(label_logits))
+        train_acc += (label_pred_class == label).sum().item() / len(label_logits)
+
+    train_loss = train_loss / len(dataloader)
+    train_acc = train_acc / len(dataloader)
+    return train_loss, train_acc
+
+def test_step(model, dataloader, loss_fn, optimizer):
+    model.eval()
+
+    test_loss, test_acc = 0, 0
+
     with torch.inference_mode():
-        test_loss = 0
-        test_acc = 0
-        for img, label in test_loader:
-            test_pred = model_0(img)
+        for batch, (img, label) in enumerate(dataloader):
+            img, label = img.to(device), label.to(device)
 
-            loss = loss_fn(test_pred.squeeze(), torch.tensor(label, dtype = torch.float))
-            test_acc += accuracy_fn(torch.round(y_pred), torch.tensor(label))
+            label_logits = model(img).squeeze()
+            loss = loss_fn(label_logits, label.float())
             test_loss += loss
-        test_loss /= len(test_loader)
-        test_acc /= len(test_loader)
 
-    
-    print(f"Loss for this epoch: {train_loss} | Train Accuracy: {acc} | Test Loss: {test_loss} | Test Accuracy: {test_acc}")
+            test_pred_labels = torch.round(torch.sigmoid(label_logits))
+            test_acc += (test_pred_labels == label).sum().item() / len(label_logits)
+
+    test_loss = test_loss / len(dataloader)
+    test_acc = test_acc / len(dataloader)
+    return test_loss, test_acc
+
+
+
+def train(model, train_dataloader, test_dataloader, acc_fn, optimizer, epochs = 5):
+
+    results = {
+        "train_loss" : [],
+        "train_acc" : [],
+        "test_loss" : [],
+        "test_acc" : []
+    }
+
+    for epoch in tqdm(range(epochs)):
+        train_loss, train_acc = train_step(model, train_dataloader, loss_fn, optimizer)
+        test_loss, test_acc = test_step(model, test_dataloader, loss_fn, optimizer)
+
+        print(
+            f"Epoch: {epoch + 1} |"
+            f"train_loss: {train_loss:.4f} | "
+            f"train_acc: {train_acc:.4f} | "
+            f"test_loss: {test_loss:.4f} | "
+            f"test_acc: {test_acc:.4f}"
+        )
+
+        results["train_loss"].append(train_loss.item() if isinstance(train_loss, torch.Tensor) else train_loss)
+        results["train_acc"].append(train_acc.item() if isinstance(train_acc, torch.Tensor) else train_acc)
+        results["test_loss"].append(test_loss.item() if isinstance(test_loss, torch.Tensor) else test_loss)
+        results["test_acc"].append(test_acc.item() if isinstance(test_acc, torch.Tensor) else test_acc)
+
+    return results
+
+model_1 = TinyVGG(3, 10, 1)
+
+torch.manual_seed(42)
+torch.cuda.manual_seed(42)
+
+loss_fn = nn.BCEWithLogitsLoss()
+optimizer = torch.optim.Adam(params=model_1.parameters(), lr=0.001)
+
+start_time = timer()
+final_results = train(model_1, train_augment_loader, test_dataloader, loss_fn, optimizer, NUM_EPOCHS)
+end_time = timer()
+print(f'Total training time: {end_time - start_time:.3f} seconds')
+
